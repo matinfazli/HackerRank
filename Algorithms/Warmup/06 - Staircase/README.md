31e73d55322bba8d7832f579d65dec07ba618c3e
Matin Fazli <matin.fazli@gmail.com>
2016-12-13 20:43:53 +0100

    Adding JS solution for Staircase

---
+++ Algorithms/Warmup/06 - Staircase/README.md
@@ -0,0 +1,46 @@
+##Staircase
+
+* __HackerRank Link:__ https://www.hackerrank.com/challenges/staircase
+* __Difficulty:__ Easy
+* __Solution(s) in:__ JavaScript
+
+=====================
+
+###Problem Statement###
+
+Consider a staircase of size _n_ = 4:
+
+```
+   #
+  ##
+ ###
+####
+```
+Observe that its base and height are both equal to _n_, and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size _n_.
+
+###Input Format###
+
+A single integer, _n_, denoting the size of the staircase.
+
+###Output Format###
+
+Print a staircase of size _n_ using # symbols and spaces.
+
+###Sample Input###
+
+```
+6
+```
+
+###Sample Output###
+
+```
+     #
+    ##
+   ###
+  ####
+ #####
+######
+```
---
+++ Algorithms/Warmup/06 - Staircase/Solution.js
@@ -0,0 +1,39 @@
+//
+// HackerRank Adventures (https://github.com/matinfazli/hackerrank)
+//
+// Author - Matin Fazli (https://github.com/matinfazli)
+// Original challenge (https://www.hackerrank.com/challenges/staircase)
+//
+
+// Original Input & Output
+// ===============
+
+// Input
+var stdin = ['6'];
+// Running main function for output
+main(stdin)
+
+
+// Solution Starts
+// ===============
+
+// Main function
+function main() {
+	// Size of the staircase
+	var n = parseInt(stdin[0]);
+
+	// Two dimensional array concept
+	for (var i = 0; i < n; i++) {
+		// Clear the output after each loop
+		var output = '';
+
+		// Loop through, whenever (n-1-i) is bigger than j concat a space else #
+		for (var j = 0; j < n; j++) {
+			j < (n-1-i) ? output += ' ' : output += '#';
+		}
+
+		// Output
+		console.log(output);
+	}
+
+}
