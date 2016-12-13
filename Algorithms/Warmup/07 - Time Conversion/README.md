c3fcbcc4a654dd7e17ca95eaabd1f132d7134a70
Matin Fazli <matin.fazli@gmail.com>
2016-12-13 21:59:43 +0100

    Adding JS solution for Time Conversion

---
+++ Algorithms/Warmup/07 - Time Conversion/README.md
@@ -0,0 +1,31 @@
+##Time Conversion
+
+* __HackerRank Link:__ https://www.hackerrank.com/challenges/time-conversion
+* __Difficulty:__ Easy
+* __Solution(s) in:__ JavaScript
+
+=====================
+
+###Problem Statement###
+
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+###Input Format###
+
+A single string containing a time in 12-hour clock format (i.e.: hh:mm:ssAM or hh:mm:ssPM), where 01 < hh < 12 and mm, ss < 59.
+
+###Output Format###
+
+Convert and print the given time in 24-hour format, where 00 < hh < 23.
+
+###Sample Input###
+
+```
+07:05:45PM
+```
+
+###Sample Output###
+
+```
+19:05:45
+```
---
+++ Algorithms/Warmup/07 - Time Conversion/Solution.js
@@ -0,0 +1,42 @@
+//
+// HackerRank Adventures (https://github.com/matinfazli/hackerrank)
+//
+// Author - Matin Fazli (https://github.com/matinfazli)
+// Original challenge (https://www.hackerrank.com/challenges/time-conversion)
+//
+
+// Original Input & Output
+// ===============
+
+// Input
+var stdin = ['07:05:45PM'];
+// Running main function for output
+main(stdin);
+
+
+// Solution Starts
+// ===============
+
+// Main function
+function main() {
+	// Convert old time string '07:05:45PM' to an array like [07,05,45,PM].
+	var time = stdin[0].match(/(\d+):(\d+):(\d+)(..)/);
+
+	// Create an object with hour, minute, second and meridian.
+	var timeObj = {	'hh': Number(time[1]), 
+									'mm': Number(time[2]), 
+                	'ss': Number(time[3]), 
+                	'meridian': time[4] };
+
+  // Change 12-Hour format to 24-Hour.
+	if (timeObj.meridian == 'PM' && timeObj.hh < 12) timeObj.hh += 12;
+	else if (timeObj.meridian == 'AM' && timeObj.hh == 12) timeObj.hh -= 12;
+
+	// If hour, minute or second value is below 10 add a 0 infront.
+	if (timeObj.hh < 10) timeObj.hh = "0" + timeObj.hh;
+	if (timeObj.mm < 10) timeObj.mm = "0" + timeObj.mm;
+	if (timeObj.ss < 10) timeObj.ss = "0" + timeObj.ss;
+
+	// Output the new formatted time.
+	console.log(timeObj.hh + ':' + timeObj.mm + ':' + timeObj.ss); 
+}
