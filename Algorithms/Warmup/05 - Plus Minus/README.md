8de5962511307f27c297f4fa68da92f5544fd9f2
Matin Fazli <matin.fazli@gmail.com>
2016-12-13 06:11:46 +0100

    Adding JS solution for Plus Minus

---
+++ Algorithms/Warmup/05 - Plus Minus/README.md
@@ -0,0 +1,39 @@
+##Plus Minus
+
+* __HackerRank Link:__ https://www.hackerrank.com/challenges/plus-minus
+* __Difficulty:__ Easy
+* __Solution(s) in:__ JavaScript
+
+=====================
+
+###Problem Statement###
+
+Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
+
+###Input Format###
+
+The first line contains an integer, N, denoting the size of the array. 
+The second line contains N space-separated integers describing an array of numbers.
+
+###Output Format###
+
+You must print the following 3 lines:
+
+1. A decimal representing of the fraction of positive numbers in the array.
+2. A decimal representing of the fraction of negative numbers in the array.
+3. A decimal representing of the fraction of zeroes in the array.
+
+###Sample Input###
+
+```
+6
+-4 3 -9 0 4 1
+```
+
+###Sample Output###
+
+```
+0.500000
+0.333333
+0.166667
+```
---
+++ Algorithms/Warmup/05 - Plus Minus/Solution.js
@@ -0,0 +1,40 @@
+//
+// HackerRank Adventures (https://github.com/matinfazli/hackerrank)
+//
+// Author - Matin Fazli (https://github.com/matinfazli)
+// Original challenge (https://www.hackerrank.com/challenges/plus-minus)
+//
+
+// Original Input & Output
+// ===============
+
+// Input
+var stdin = ['6', '-4 3 -9 0 4 1'];
+// Running main function for output
+main(stdin)
+
+
+// Solution Starts
+// ===============
+
+// Main function
+function main() {
+	// Size of the array
+	var n = stdin[0];
+	// Creating an array of integers from the input
+	var arr = stdin[1].split(' ').map(Number);
+	// Initializing variables for positive, negative and zero counts.
+	var pCount = 0, nCount = 0, zCount = 0, sum = arr.length;
+
+	// Looping through the array and counting.
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) pCount += 1;
+		else if (arr[i] < 0) nCount += 1;
+		else zCount += 1;
+	}
+
+	// Output
+	console.log(Number(pCount / sum).toFixed(6));
+	console.log(Number(nCount / sum).toFixed(6));
+	console.log(Number(zCount / sum).toFixed(6));
+}
