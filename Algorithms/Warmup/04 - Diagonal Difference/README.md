cd68a008c0b8932fe77c124daa124d03bafe9045
Matin Fazli <matin.fazli@gmail.com>
2016-12-13 05:54:53 +0100

    Adding JS solution for Diagonal Difference

---
+++ Algorithms/Warmup/04 - Diagonal Difference/README.md
@@ -0,0 +1,34 @@
+##Diagonal Difference
+
+* __HackerRank Link:__ https://www.hackerrank.com/challenges/diagonal-difference
+* __Difficulty:__ Easy
+* __Solution(s) in:__ JavaScript
+
+=====================
+
+###Problem Statement###
+
+Given a square matrix of size N x N, calculate the absolute difference between the sums of its diagonals.
+
+###Input Format###
+
+The first line contains a single integer, N. The next N lines denote the matrix's rows, with each line containing N space-separated integers describing the columns.
+
+###Output Format###
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
+
+###Sample Input###
+
+```
+3
+11 2 4
+4 5 6
+10 8 -12
+```
+
+###Sample Output###
+
+```
+15
+```
---
+++ Algorithms/Warmup/04 - Diagonal Difference/Solution.js
@@ -0,0 +1,47 @@
+//
+// HackerRank Adventures (https://github.com/matinfazli/hackerrank)
+//
+// Author - Matin Fazli (https://github.com/matinfazli)
+// Original challenge (https://www.hackerrank.com/challenges/a-very-big-sum)
+//
+
+// Original Input & Output
+// ===============
+
+// Input
+var stdin = ['3', '11 2 4', '4 5 6', '10 8 -12'];
+// Running main function for output
+main(stdin)
+
+
+// Solution Starts
+// ===============
+
+// Main function
+function main() {
+	// Size of the array
+	var n = stdin[0];
+	
+	// Initialize new array
+	var arr = [];
+	// Looping through input, creating and adding an array of integers for each.
+	for (var i = 1; i < stdin.length; i++) {
+		arr.push(stdin[i].split(' ').map(Number));
+	}
+	
+	// Initializing variables
+	var sum = 0, priSum = 0, secSum = 0;
+	// Looping through the array and summing the diagonals.
+	for (var j = 0; j < arr.length; j++) {
+		// Calculating the primary diagonal.
+		priSum += arr[j][j];
+		// Reversing the second dimension of array to calculate secondary diagonal.
+		secSum += arr[j][Math.abs(j - (arr.length - 1))];
+	}
+
+	// Calculating the absolute difference
+	sum = Math.abs(priSum - secSum);
+
+	// Output
+	console.log(sum);
+}
