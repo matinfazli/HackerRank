edab43bc260d26c3260e61d480bee445744a04a1
Matin Fazli <matin.fazli@gmail.com>
2016-12-15 08:16:08 +0100

    Adding JS solution for Divisible Sum Pairs

---
+++ Algorithms/Implementation/06 - Divisible Sum Pairs/README.md
@@ -0,0 +1,39 @@
+##Divisible Sum Pairs
+
+* __HackerRank Link:__ https://www.hackerrank.com/challenges/divisible-sum-pairs
+* __Difficulty:__ Easy
+* __Solution(s) in:__ JavaScript
+
+=====================
+
+###Problem Statement###
+
+You are given an array of _n_ integers, _a0_, _a1_,..., _an-1_, and a positive integer, _k_. Find and print the number of (_i_, _j_)pairs where _i_ < _j_ and _ai_ + _aj_ is evenly divisible by _k_.
+
+###Input Format###
+
+The first line contains 2 space-separated integers, _n_ and _k_, respectively. 
+The second line contains _n_ space-separated integers describing the respective values of _a0_, _a1_,..., _an-1_.
+
+###Constraints###
+
+* 2 < _n_ < 100
+* 1 < _k_ < 100
+* 1 < _ai_ < 100
+
+###Output Format###
+
+Print the number of (_i_, _j_) pairs where _i_ < _j_ and _ai_ + _aj_ is evenly divisible by _k_.
+
+###Sample Input###
+
+```
+6 3
+1 3 2 6 1 2
+```
+
+###Sample Output###
+
+```
+5
+```
---
+++ Algorithms/Implementation/06 - Divisible Sum Pairs/Solution.js
@@ -0,0 +1,41 @@
+//
+// HackerRank Adventures (https://github.com/matinfazli/hackerrank)
+//
+// Author - Matin Fazli (https://github.com/matinfazli)
+// Original challenge (https://www.hackerrank.com/challenges/divisible-sum-pairs)
+//
+
+// Original Input & Output
+// =======================
+
+// Input
+var stdin = ['6 3', '1 3 2 6 1 2'];
+// Running main function for output
+main(stdin);
+
+
+// Solution Starts
+// ===============
+// IMP: In HackerRank replace stdin(s) with readLine()
+
+// Main function
+function main() {
+    // Get inputs and turn them into array of integers.
+    var nk = stdin[0].split(' ').map(Number),
+        arr = stdin[1].split(' ').map(Number);
+
+    // Initialize required variables
+    var i, j, k = nk[1], count = 0;
+    
+    // Double loop through the numbers
+    for (i in arr) {
+        for (j in arr) {
+            // If its bigger than the current loop count and 
+            // the i, j pair divided by k has 0 remainder, +1 count.
+            if (i < j && (arr[i] + arr[j]) % k == 0) count++; 
+        }
+    }
+    
+    // Output
+    console.log(count);
+}
